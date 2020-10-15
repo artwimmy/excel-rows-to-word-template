d523839cdecacb39a00b5919b1b90cca8f847868
Artim Dauti <twimy.artim@gmail.com>
2020-10-15 22:23:29 +0200

    Initial commit of the converter C# app

---
+++ WordReport/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WordReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(502, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Report";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(72, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Choose Excel File";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(208, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(453, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(72, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Choose Word Template";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(249, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(411, 28);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(37, 220);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Results folder path";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(238, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(422, 28);
+            this.textBox3.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(72, 276);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(588, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Excel Rows to Word Template Documents";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(722, 386);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WordReport/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+// com objects
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WordReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+/*            string res = "C:\\Users\\twimy\\OneDrive\\Desktop\\FINKI\\iSolve\\C# Word templates\\Form.xlsx";
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(res, 0, true, 5, "", "", true);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            */
+                                             
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+
+            //dokumenti excel per numrimin e reshtave...
+            string exceldoc = "C:\\Users\\twimy\\OneDrive\\Desktop\\FINKI\\iSolve\\C# Word templates\\Form.xlsx";
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook sheet = excel.Workbooks.Open(exceldoc);
+            Microsoft.Office.Interop.Excel.Worksheet x = excel.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+
+            Excel.Range userRange = x.UsedRange;
+            int countRecords = userRange.Rows.Count;
+            //int add = countRecords +1;
+            //x.Cells[add, 1] = "Total Rows "+countRecords;
+
+            //progress bar
+            this.progressBar1.Maximum = 100;
+
+            //dokumenti template
+            Microsoft.Office.Interop.Word.Document doc = null;
+            object fileName = textBox2.Text;
+            //object fileName = "C:\\Users\\twimy\\OneDrive\\Desktop\\FINKI\\iSolve\\C# Word templates\\Template.docx";
+            object missing = Type.Missing;
+
+            for(int i=0; i<countRecords; i++)
+            {
+                doc = app.Documents.Open(fileName, missing, missing);
+                app.Selection.Find.ClearFormatting();
+                app.Selection.Find.Replacement.ClearFormatting();
+
+                string[] tmp = new string[4];
+                tmp = readExcel(i);
+
+                app.Selection.Find.Execute("<ID>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[0],2);
+                app.Selection.Find.Execute("<name>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[1],2);
+                app.Selection.Find.Execute("<sex>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[2],2);
+                app.Selection.Find.Execute("<age>", missing, missing, missing, missing, missing, missing, missing, missing, tmp[3],2);
+
+
+                object SaveAsFile = (object) textBox3.Text + tmp[0] + ".doc";
+                doc.SaveAs2(SaveAsFile, missing, missing, missing);
+                this.progressBar1.Value += (100 / countRecords);
+            }
+            this.progressBar1.Value = 100;
+            MessageBox.Show("Fajllat u krijuan me sukses!");
+            doc.Close(false, missing, missing);
+            app.Quit(false, false, false);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+            sheet.Close(true, Type.Missing, Type.Missing);
+            excel.Quit();
+        }
+
+        // read data from the excel
+        private string[] readExcel(int index)
+        {
+            //string res = "C:\\Users\\twimy\\OneDrive\\Desktop\\FINKI\\iSolve\\C# Word templates\\Form.xlsx";
+            string res = textBox1.Text;
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            //int rowCount = xlWorkSheet.Rows.Count();
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(res, 0, true, 5, "", "", true);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            index += 2;
+
+            string[] data = new string[4]; // {ID, EMRI, GJINIA, MOSHA
+            data[0] = xlWorkSheet.get_Range("A" + index.ToString()).Text; // ID
+            data[1] = xlWorkSheet.get_Range("B" + index.ToString()).Value; // emri
+            data[2] = xlWorkSheet.get_Range("C" + index.ToString()).Value; // gjinia
+            data[3] = xlWorkSheet.get_Range("D" + index.ToString()).Text; // mosha
+            
+            xlWorkBook.Close(false);
+            xlApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWorkSheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWorkBook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlApp);
+
+            return data;
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "EXCEL|*.xlsx";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+            }
+        }
+
+        OpenFileDialog wrd = new OpenFileDialog();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wrd.Filter = "WORD|*.docx";
+            if(wrd.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = wrd.FileName;
+            }
+        }
+
+        FolderBrowserDialog reps = new FolderBrowserDialog();
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (reps.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = reps.SelectedPath;
+            }
+        }
+    }
+}
